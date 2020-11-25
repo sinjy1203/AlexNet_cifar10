d3d83e04a9b0bb7104313dec703e2c67749f0748
sinjy <sinjy1203@gmail.com>
2020-11-25 10:30:04 +0900

    shin

--- dataset.py
+++
@@ -1,9 +0,0 @@
-import torch
-import torch.nn as nn
-import numpy as np
-
-from torch.utils.data import DataLoader
-from torchvision import datasets, transforms
-
-class Dataset(torch.utils.data.Dataset):
-    def __init__(self, ):
